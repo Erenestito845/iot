9039bcf3b0da5d021d6e610ad923dbc4bd371c05
Gabriel Matías Acuña Heredia <gabriel.acuna13@inacapmail.cl>
2025-11-01 20:40:20 -0300

    ya sabemos mas sobre como trancicionar entre ventana en ventana.

--- app/src/main/java/com/example/evaluacin_iot/Act_login.kt
+++ app/src/main/java/com/example/evaluacin_iot/Act_login.kt
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 private lateinit var btningresar: Button
+private lateinit var btnregistrar: Button
+
 class Act_login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +21,14 @@ class Act_login : AppCompatActivity() {
             insets
         }
         btningresar = findViewById(R.id.btn_ingresar)
+        btnregistrar = findViewById(R.id.btn_registrar)
 
         btningresar.setOnClickListener{
             val intent = Intent(this, Act_Menu_Crud::class.java)
+            startActivity(intent)
+        }
+        btnregistrar.setOnClickListener{
+            val intent = Intent(this, registro_usuario::class.java)
             startActivity(intent)
         }
     }
